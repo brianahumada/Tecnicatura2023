b5f5abb38e0863bbceb75dc9970ba5ddfb1a7a39
brian ahumada <ahumada.yayan@gmail.com>
2023-05-15 20:31:44 -0300

    Subo clase 5 de javaScript

--- JavaScript/leccion4/01-04-objetos.js
+++ JavaScript/leccion4/01-04-objetos.js
@@ -8,9 +8,23 @@ let persona = {
     apellido: 'Gil',
     email: 'cgil@gmail.com',
     edad: 25,
+    idioma : "es",
     nombreCompleto: function(){//método o función en javaScript
         return this.nombre+' '+this.apellido;
+    },
+    //Mostramos
+    get nombreEdad(){//Metodo get
+        return 'El nombre es: '+this.nombre+', edad: '+ this.edad;        
+    },
+    //Get MAYUSCULA
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+    //set
+    set lang(Lang){
+        this.idioma = Lang.toUpperCase();
     }
+
 }
 
 console.log(persona.nombre);
@@ -63,3 +77,102 @@ console.log(personaArray);
 console.log("Distintas formas de imprimir un objeto: forma 4");
 let personaString = JSON.stringify(persona);
 console.log(personaString);
+
+//Clase5 mostramos el metodo get
+console.log('comenzamos a utilizar el método get')
+console.log(persona.nombreEdad);
+
+console.log('Comenzamos con el método gety set para idiomas');
+persona.lang = 'en'
+console.log(persona.lang);
+
+function Persona3(nombre, apellido, email){ //Function Constructor para persona
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.nombreCompleto = function(){ //Metodos dentro del constructor 
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+let padre = new Persona3('Leo', 'Lopez', 'Lopez@gmail.com');
+padre.nombre = 'Luis'
+padre.telefono = '2625594512';
+console.log(padre);
+console.log(padre.nombreCompleto());
+
+let madre = new Persona3('Laura', 'Contrera', 'Contrera@gmail.com');
+console.log(madre);
+console.log(madre.nombreCompleto());
+
+//Diferentes formas de crear objetos
+//caso número 1
+let miObjeto = new Object();
+//caso 2
+let miObjeto2 = {};
+
+//caso String1
+let miCaddena1 = new String('Hola'); //Sintaxis formal
+//caso String2
+let miCaddena2 = 'hola';//Esta es la sintaxos simplificada y recomendada
+
+//caso con numeros 1
+let numero1 = new Number(1);//NO RECOMENDABLE
+//caso 2
+let numero2 = 1;//RECOMENDADA
+
+//caso booleano 1
+let miBoleano1 = new Boolean(true);//NO RECOMENDAB
+//caso 2
+let miBoleano2 = true;//RECOMENDADA
+
+//caso arreblos 1
+let arreglo1 = new Array();//NO RECOMENDABLE
+//caso 2
+let arreglo2 = [];//RECOMENDADA
+
+//caso function 1
+let func1 = new Function();//NO RECOMENDABLE
+//caso 2
+let func2 = function(){};//RECOMENDADA
+
+//uso de prototype
+Persona3.prototype.telefono = '3659485163';/*
+Se está agregando una nueva propiedad llamada telefono al prototipo de la función constructora Persona3.
+Esto significa que todos los objetos creados a partir de Persona3 heredarán esta propiedad y podrán acceder a ella.. */
+console.log(padre);
+//madre
+console.log(madre);
+madre.telefono = '723643782'
+console.log(madre.telefono);
+
+//Uso de call
+/* 
+El método call() nos permite cambiar el contexto de ejecución de una función y especificar el valor de this 
+que se utilizará dentro de la función. 
+Esto permite reutilizar métodos en diferentes objetos y obtener resultados personalizados según el contexto.
+*/
+let persona4 = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    nombreCompleto2: function(titulo, telefono) {
+        return titulo+': '+this.nombre+' '+this.apellido+' '+telefono;
+        //return this.nombre+' '+this.apellido;
+    }
+}
+
+let persona5 = {
+    nombre: 'Pedro',
+    apellido: 'landa',
+}
+
+console.log(persona4.nombreCompleto2('lic.', '345346645'));
+console.log(persona4.nombreCompleto2.call(persona5,'Ing.','43534654'));/*
+Al usar call(persona5, 'Ing.', '43534654'), establecemos persona5 como el valor de this dentro de la 
+función nombreCompleto2. Ahora, this dentro de nombreCompleto2 se refiere a persona5, y los valores de nombre 
+y apellido se obtienen de persona5. Por lo tanto, el resultado es diferente y se imprime "Ing.: Pedro landa 43534654".
+*/
+
+
+//Método  apply() acepta un arreglo de argumentos.
+let arreglo = ['Ing.', '154646512'];
+console.log(persona4.nombreCompleto2.apply(persona5, arreglo));
